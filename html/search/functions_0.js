2f4073d5493e3a23f641461f4ee58c2c9387f9a4
mikolaj2002 <41815719+mikolaj2002@users.noreply.github.com>
2022-05-29 22:20:06 +0200

    Add files via upload

---
+++ html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sertsquareheight_0',['sertSquareHeight',['../class_board.html#aac467ea880a3bd28a89a1e5f5aa900cd',1,'Board']]],
+  ['setsquarewidth_1',['setSquareWidth',['../class_board.html#a9c7c60c2a525fbe0395cb2988d7bd51b',1,'Board']]]
+];
